814fc91e047b6931571c209ca935d8a050fe8e6a
GabrielVornicu <33860059+GabrielVornicu@users.noreply.github.com>
2026-03-02 21:40:52 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,9 @@
-const express = require("express");
-const path = require("path");
+require("dotenv").config();
 
-const app = express();
+const app = require("./app");
+
 const PORT = process.env.PORT || 3000;
 
-// 🔥 ADAUGĂ ASTA (obligatoriu pentru formulare)
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
-
-// Import routes
-const indexRoutes = require("./routes/index");
-app.use("/", indexRoutes);
-
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on port " + PORT);
+  console.log("🚀 Server running on port " + PORT);
 });
