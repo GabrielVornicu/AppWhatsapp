9bde8fac1cd0338d80f19d818f11698c4bbd1bad
GabrielVornicu <33860059+GabrielVornicu@users.noreply.github.com>
2026-03-02 12:57:36 +0200

    Update logger.js

--- src/config/logger.js
+++ src/config/logger.js
@@ -6,13 +6,15 @@ const logger = createLogger({
     format.timestamp(),
     format.errors({ stack: true }),
     format.printf(({ timestamp, level, message, stack }) => {
-      return `${timestamp} [${level.toUpperCase()}] ${stack || message}`;
+      return stack
+        ? `${timestamp} [${level}] ${message} - ${stack}`
+        : `${timestamp} [${level}] ${message}`;
     })
   ),
   transports: [
     new transports.Console(),
-    new transports.File({ filename: "logs/error.log", level: "error" }),
-    new transports.File({ filename: "logs/combined.log" }),
+    new transports.File({ filename: "error.log", level: "error" }),
+    new transports.File({ filename: "combined.log" }),
   ],
 });
 
