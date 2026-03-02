33b53e34d455de05f5bfcfd65898ca56a09cf23a
GabrielVornicu <33860059+GabrielVornicu@users.noreply.github.com>
2026-03-02 21:18:45 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// 🔥 ADAUGĂ ASTA (obligatoriu pentru formulare)
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
