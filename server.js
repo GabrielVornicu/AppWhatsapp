f2e046bd3311b668bcf8a21d8e80d7a514741cbd
GabrielVornicu <33860059+GabrielVornicu@users.noreply.github.com>
2026-03-03 14:53:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 
 const app = require("./app");
+const { startQueueRunner } = require("./workers/queueRunner");
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("🚀 Server running on port " + PORT);
+
+  // pornește worker-ul de mesaje
+  startQueueRunner();
 });
